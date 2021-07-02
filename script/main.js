56942277da47ede7a27bd13af47ba3cc46395dde
Rashidov01 <avazbekrashidov6@gmail.com>
2021-07-02 10:51:04 +0500

    initial commit

---
+++ script/main.js
@@ -0,0 +1,8 @@
+let elHeader = document.querySelector('.header');
+let hamburgMenu = document.querySelector('.js-toggle__button'); 
+
+if (hamburgMenu){
+  hamburgMenu.addEventListener('click', function(){
+    elHeader.classList.toggle('site-header__open')
+  })
+}
